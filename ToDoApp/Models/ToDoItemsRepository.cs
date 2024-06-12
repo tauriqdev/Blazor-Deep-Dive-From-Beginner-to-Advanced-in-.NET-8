2fb7820d8e6c0047257001a6b70034ffc9c75561
Tauriq Hendricks <tauriqh786@gmail.com>
2024-06-12 15:27:08 +0200

    Mark task as completed use case

--- ToDoApp/Models/ToDoItemsRepository.cs
+++ ToDoApp/Models/ToDoItemsRepository.cs
@@ -7,7 +7,7 @@
             new() { Id = 1, Name = "Item 1"},
             new() { Id = 2, Name = "Item 2"},
             new() { Id = 3, Name = "Item 3"},
-            new() { Id = 4, Name = "Item 4"}
+            new() { Id = 4, Name = "Item 4", IsCompleted=true}
         };
 
         public static void AddItem(ToDoItem item)
