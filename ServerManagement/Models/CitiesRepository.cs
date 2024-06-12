7bfdcf625a832c71b74906d23368c8fa409f0a3e
Tauriq Hendricks <tauriqh786@gmail.com>
2024-06-12 10:07:50 +0200

    Add project files.

---
+++ ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepository
+    {
+        private static readonly List<string> _cities =
+        [
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax",
+        ];
+
+        public static List<string> GetCities() => _cities;
+    }
+}
