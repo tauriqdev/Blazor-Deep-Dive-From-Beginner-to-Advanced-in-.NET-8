4776d52083621f52cabecced2c4381172e962ff6
Tauriq Hendricks <tauriqh786@gmail.com>
2024-06-19 15:16:32 +0200

    Create DBContext

---
+++ ServerManagement/Data/ServerManagementContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServerManagement.Models;
+
+namespace ServerManagement.Data
+{
+    public class ServerManagementContext : DbContext
+    {
+        public DbSet<Server> Servers { get; set; }
+
+
+        // when model is being created do
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Server>().HasData(
+                new() { Id = 1, Name = "Server1", City = "Toronto" },
+                new() { Id = 2, Name = "Server2", City = "Toronto" },
+                new() { Id = 3, Name = "Server3", City = "Toronto" },
+                new() { Id = 4, Name = "Server4", City = "Montreal" },
+                new() { Id = 5, Name = "Server5", City = "Montreal" },
+                new() { Id = 6, Name = "Server6", City = "Montreal" },
+                new() { Id = 7, Name = "Server7", City = "Ottawa" },
+                new() { Id = 8, Name = "Server8", City = "Ottawa" },
+                new() { Id = 9, Name = "Server9", City = "Ottawa" },
+                new() { Id = 10, Name = "Server10", City = "Calgary" },
+                new() { Id = 11, Name = "Server11", City = "Calgary" },
+                new() { Id = 12, Name = "Server12", City = "Halifax" },
+                new() { Id = 13, Name = "Server13", City = "Halifax" },
+                new() { Id = 14, Name = "Server14", City = "Halifax" },
+                new() { Id = 15, Name = "Server15", City = "Halifax" }
+            );
+        }
+    }
+}
